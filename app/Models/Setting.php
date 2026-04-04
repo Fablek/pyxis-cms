3d8ebf39392a21a0e7e3ceb62cd469516dc76f12
Sebastian Pytka <pytka96@gmail.com>
2026-04-04 09:17:01 +0200

    feat: add cache for setting model

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,18 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Setting extends Model
 {
     protected $fillable = ['key', 'value'];
 
+    private static string $cachePrefix = 'setting_';
+
+    protected static function booted(): void
+    {
+        static::saved(function (Setting $setting) {
+            Cache::forget(self::$cachePrefix . $setting->key);
+        });
+
+        static::deleted(function (Setting $setting) {
+            Cache::forget(self::$cachePrefix . $setting->key);
+        });
+    }
+
     public static function get(string $key, $default = null) 
     {
-        return static::where('key', $key)->first()?->value ?? $default;
+        return Cache::rememberForever(self::$cachePrefix . $key, function () use ($key, $default) {
+            return static::where('key', $key)->first()?->value ?? $default;
+        });
     }
 
     public static function set(string $key, $value)
     {
-        return static::updateOrCreate(['key' => $key], ['value' => $value]);
+        $setting = static::updateOrCreate(['key' => $key], ['value' => $value]);
+
+        Cache::forget(self::$cachePrefix . $key);
+
+        return $setting;
     }
 }
