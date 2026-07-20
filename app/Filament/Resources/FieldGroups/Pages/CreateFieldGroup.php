68820c3ddfa7413d93b51e0f5774d4b8e3aa1fe2
Sebastian Pytka <pytka96@gmail.com>
2026-07-20 20:50:02 +0200

    style: field gropus - move buttons to nav

--- app/Filament/Resources/FieldGroups/Pages/CreateFieldGroup.php
+++ app/Filament/Resources/FieldGroups/Pages/CreateFieldGroup.php
@@ -3,9 +3,37 @@
 namespace App\Filament\Resources\FieldGroups\Pages;
 
 use App\Filament\Resources\FieldGroups\FieldGroupResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFieldGroup extends CreateRecord
 {
     protected static string $resource = FieldGroupResource::class;
+
+    public function getHeaderActions(): array
+    {
+        return [
+            Action::make('create')
+                ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
+                ->color('primary')
+                ->action('create')
+                ->keyBindings(['mod+s']),
+
+            Action::make('createAnother')
+                ->label(__('filament-panels::resources/pages/create-record.form.actions.create_another.label'))
+                ->color('gray')
+                ->action('createAnother')
+                ->keyBindings(['mod+shift+s']),
+
+            Action::make('cancel')
+                ->label(__('filament-panels::resources/pages/create-record.form.actions.cancel.label'))
+                ->color('gray')
+                ->url($this->getResource()::getUrl('index')),
+        ];
+    }
+
+    public function getFormActions(): array
+    {
+        return [];
+    }
 }
