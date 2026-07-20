ab9de934534c49fc91248faf30693aec7bac3d62
Sebastian Pytka <pytka96@gmail.com>
2026-07-20 20:15:59 +0200

    feat: make migration - field_gropus table

---
+++ database/migrations/2026_07_20_201043_create_field_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('field_groups', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('title');
+            $table->string('slug')->unique();
+
+            $table->jsonb('fields')->nullable();
+
+            $table->jsonb('rules')->nullable();
+
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('field_groups');
+    }
+};
